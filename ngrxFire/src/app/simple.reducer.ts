e4b6c8faedd24a3614d1c78769550536628c0e46
RukshanDias <rukshandias002@gmail.com>
2023-10-12 19:34:24 +0530

    updated the reducer

--- ngrxFire/src/app/simple.reducer.ts
+++ ngrxFire/src/app/simple.reducer.ts
@@ -1,16 +1,14 @@
 import { Action } from '@ngrx/store';
 
 export function simpleReducer(state: string = 'Hello World', action: Action) {
-
   switch (action.type) {
-
-		case 'SPANISH':
-		  return state = 'Hola Mundo'
+    case 'SPANISH':
+      return (state = 'This is Spanish');
 
     case 'FRENCH':
-      return state = 'Bonjour le monde'
+      return (state = 'This is French');
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 }
